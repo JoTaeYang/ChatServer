f1e7396b450e504081d10745326a1cf0114484ea
yangdole\yandole <whxodid666@naver.com>
2025-02-25 20:31:04 +0900

    # 백준 풀이 안맞던거 다시 수정

--- BaekJoon/1912 연속합/1912.cpp
+++ BaekJoon/1912 연속합/1912.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 int main()
 {
-	int n, k;
+	int n;
 
-	std::cin >> n >> k;
+	std::cin >> n;
 
 	std::vector<int> array(n);
 	std::vector<int> Total(n);
-
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; i++)
 		std::cin >> array[i];
 
-	int checkIdx = k - 1;
-	int j = 0;
-	Total[0] = array[0];
-	for (int i = 1; i < n; ++i)
+	/*
+	loop를 돌면서 수를 더하다가
+	어떤 기준을 두고 이 수를 버려서
+	다시 수열을 시작을 해야함
+
+	기준을 어떻게 잡는가가? 관건인듯하다
+
+	10 6
+	6이 되는 순간 수열을 버려야함.
+	왜 버려야 하나? 이전 값보다 작아짐.
+
+	근데 수열을 다시 시작할 때, 거의 높은 확률로 이전 값보다 작을 거라 거기서부터 고민이 시작
+	*/
+	int num = 0;
+	int total = 0;
+	int maxTotal = array[0];
+	for (int i = 0; i < n; i++)
 	{
-		if (i > checkIdx)
-		{
-			Total[i] = Total[i - 1] + array[i] - array[j];
-			j++;
-		}
-		else
-			Total[i] = Total[i - 1] + array[i];
+		if (total < 0)
+			total = 0;
+
+		maxTotal = std::max(maxTotal, total + array[i]);
+		total += array[i];
 	}
 
-	std::sort(Total.begin() + checkIdx, Total.end());
-
-	std::cout << *(Total.end() - 1);
+	std::cout << maxTotal;
 
 	return 0;
 }
